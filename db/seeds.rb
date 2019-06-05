d0a6ec4a70a38a3be0627382bbe78b75e5b50378
luigi manganiello <luigi.manga92@gmail.com>
2019-06-05 16:58:26 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# name = Faker::Movies::HarryPotter.character
+# address = Faker::Movies::HarryPotter.location
+# phone = Faker::Movies::HarryPotter.house
+# cat = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do
+  name = Faker::Movies::HarryPotter.character
+  address = Faker::Movies::HarryPotter.location
+  phone = Faker::Movies::HarryPotter.house
+  cat = ["chinese", "italian", "japanese", "french", "belgian"]
+  Restaurant.new(name: name, address: address, phone_number: phone, category: cat.sample).save
+end
